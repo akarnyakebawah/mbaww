b0b98989f49b7fb6bf427e7573fc70cb983076ae
Bernino Falya <falya.bernino@gmail.com>
2018-09-16 18:13:48 +0000

    added new sample campaign

--- server-full.js
+++ server-full.js
@@ -5,4 +5,9 @@ app.get('/', (req,res) => res.sendFile(__dirname + '/index.html'))
 
 app.use('/campaign', express.static(__dirname + '/campaign'))
 app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/tumpenganmode2017', express.static(__dirname + '/campaign/tumpenganmode2017'))
+app.use('/m2m', express.static(__dirname + '/campaign/m2m'))
+
+
+
 app.listen(3000, () => console.log('running'))
