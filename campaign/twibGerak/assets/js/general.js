9f679225ac49808b6c0b08978cb552afb012de9c
Bernino Falya <falya.bernino@gmail.com>
2018-09-17 18:44:16 +0700

    added twibbon gerak front-end

---
+++ campaign/twibGerak/assets/js/general.js
@@ -0,0 +1,125 @@
+// ---- nama twibbon (tanpa format) -----
+var twibbon = "aspectratio";
+
+(function($) {
+
+    var result = $('#result');
+    var coordinates;
+    var canvas = document.getElementById("uploadimage");
+    var context = canvas.getContext("2d");
+
+    function processImage(img) {
+      $('.image-hide, .text-content-2, .new-download').show();
+      $('h2, figure, .attachment-1, .text-content-1').hide();
+      $('.images-caman').addClass('images-caman-active');
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+        var imageData = context.getImageData(0, 0,  canvas.width,  canvas.height);
+        var px = imageData.data;
+        var len = px.length;
+        context.putImageData(imageData, 0, 0);
+
+        $('#uploadimage').Jcrop({
+          bgOpacity:   .4,
+          allowResize: false,
+          allowSelect: false,
+          setSelect:   [ 0, 0, 800, 800 ],
+          aspectRatio: 1,
+          onSelect: function (coords) {
+            coordinates = coords
+          },
+          onRelease: function () {
+            coordinates = null
+          }
+        });
+    $('#statusedit').hide();
+    }
+
+    function displayImage (file, options) {
+      currentFile = file
+      if (!loadImage(
+          file,
+          processImage,
+          options
+        )) {
+          alert("Your browser does not support the URL or FileReader API");
+        }
+    }
+
+    function dropChangeHandler (e) {
+      e.preventDefault()
+      e = e.originalEvent
+      var target = e.dataTransfer || e.target
+      var file = target && target.files && target.files[0]
+      var options = {
+          maxWidth: 800,
+          canvas: true,
+          pixelRatio: window.devicePixelRatio,
+          downsamplingRatio: 0.5
+      }
+      if (!file) {
+          return
+      }
+
+      loadImage.parseMetaData(file, function (data) {
+          if (data.exif) {
+            options.orientation = data.exif.get('Orientation')
+          }
+          displayImage(file, options)
+      })
+    }
+
+  $('#attachment').on('change', dropChangeHandler)
+
+  var downloadBtn = document.getElementById('download');
+  var loadingScreen = document.getElementById('loading');
+  var preview = document.getElementById('preview');
+  var previewBtn = document.getElementById('previewBtn');
+ 
+
+  //var img2 = loadImage('./assets/images/pigura.png');
+  var crop_canvas = document.createElement('canvas');
+  crop_canvas.width = 800;
+  crop_canvas.height = 800;
+  downloadBtn.addEventListener('click', function() {
+
+      
+       var ratioY = canvas.height / result.height(),
+             ratioX = canvas.width / result.width();
+          var getX = coordinates.x * ratioX,
+             getY = coordinates.y * ratioY,
+             getWidth = coordinates.h * ratioX,
+             getHeight = coordinates.w * ratioY;
+
+        crop_canvas.getContext('2d').drawImage(canvas, getX,getY,getWidth,getHeight,0,0,800,800);
+        // crop_canvas.getContext('2d').drawImage(img2, 0,0,800,800,0,0,800,800);
+        crop_canvas.toBlobHD(function(blob) {
+          //  saveAs(blob,"#tumpenganmodeikj2017.png");
+          var formData = new FormData();
+          formData.append('image', blob); 
+          formData.append('twibbon', twibbon);
+
+          var oReq = new XMLHttpRequest();
+          oReq.open('POST', '/convert/', true); 
+          oReq.responseType = 'json'; 
+          oReq.onload = function() {
+            var jsonResponse = oReq.response; 
+
+            console.log(jsonResponse); 
+             
+            setTimeout(function () { window.location = window.location.host+'result/'+jsonResponse.filename; }, 5000)
+          }
+          oReq.send(formData);
+
+          loadingScreen.style.display = 'block';
+          preview.style.display = 'none';
+          previewBtn.style.display = 'none';
+          console.log('send nih');
+        }, "image/jpg");
+
+    }, false);
+
+
+})(jQuery);
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const upload = multer({storage : storage})
 
 const convertVidFile = (vidfile, res) => {
 	vidfilemp4 = vidfile.split('.')[0].concat('.mp4')
-	exec('ffmpeg -i .\\result\\'+vidfile+' -r 30 .\\result\\'+vidfilemp4, (err, stdout, stderr) => {
+	exec('ffmpeg -i ./result/'+vidfile+' -r 30 ./result/'+vidfilemp4, (err, stdout, stderr) => {
 		if(err){return;} 
 		res.setHeader('Content-Type', 'application/json') 
 		res.json({filename : vidfilemp4})
@@ -33,14 +33,23 @@ const convertVidFile = (vidfile, res) => {
 app.get('/', (req,res) => res.sendFile(path.join(__dirname + '/index.html')))
 app.post('/convert', upload.single('image'), (req,res,next) => {
 	let imgfile = req.file.filename
+	let twibbon = req.body.twibbon
+	console.log(imgfile)
 	let vidfile = imgfile.split('.')[0].concat('.avi')
-	exec('python .\\conv.py .\\twibbon_vid\\animecut.mp4 .\\tmp\\'+imgfile+' .\\result\\'+vidfile, (err, stdout, stderr) => {
-		if(err){return;} 
-		convertVidFile(vidfile, res)
+	exec('python ./conv.py ./twibbon_vid/'+twibbon+'.mp4 ./tmp/'+imgfile+' ./result/'+vidfile, (err, stdout, stderr) => {
+	 	if(err){return;} 
+	 	convertVidFile(vidfile, res)
 	})
 })
 
-app.use('/result', express.static('result'))
-app.use('/lib', express.static('lib'))
-app.use('/assets', express.static('assets'))
-app.listen(3220, () => console.log('Twiggsy Running'))
+app.use('/result', express.static(__dirname + '/result'))
+app.use('/lib', express.static(__dirname + '/lib'))
+app.use('/assets', express.static(__dirname + '/assets'))
+
+// Active Campaign :
+app.use('/campaign', express.static(__dirname + '/campaign'))
+app.use('/tumpenganmode2017', express.static(__dirname + '/campaign/tumpenganmode2017'))
+app.use('/m2m', express.static(__dirname + '/campaign/m2m'))
+
+
+app.listen(3000, () => console.log('Twiggsy Running'))
